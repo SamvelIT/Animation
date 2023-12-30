755e900faa3ef0d96c046f2f700011a292feaf77
SamvelIT <babadzanan98@mail.ru>
2023-12-30 14:18:37 +0300

    PROJECT

---
+++ main2.js
@@ -0,0 +1,14 @@
+console.clear();
+gsap.config({ trialWarn: false });
+gsap.registerPlugin(ScrollTrigger);
+gsap.to("#container", {
+  "--target": "0%",
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#container",
+    start: "top top",
+    end: "+=1000",
+    pin: true,
+    scrub: 1
+  }
+});
